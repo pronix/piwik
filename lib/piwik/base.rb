55c234792b8752bd1204f3960f3f6d3b7bb2cef8
Damir Sharipov <dammer2k@gmail.com>
2010-05-17 16:49:55 +0400

    Изменил расположение конфига для рельсов, добавил get_page_urls

--- lib/piwik/base.rb
+++ lib/piwik/base.rb
@@ -12,31 +12,44 @@ module Piwik
 
   class Base
     @@template  = <<-EOF
-# .piwik
-# 
+# piwik.yml
+#
 # Please fill in fields like this:
 #
 #  piwik_url: http://your.piwik.site
 #  auth_token: secret
 #
-piwik_url: 
-auth_token: 
+development:
+  piwik_url:
+  auth_tokien:
+
+production:
+  piwik_url:
+  auth_tokien:
+
+test: &TEST
+  piwik_url:
+  auth_tokien:
+
+cucumber:
+  <<: *TEST
+
 EOF
-      
+
     private
-      # Checks for the config, creates it if not found
+    # Checks for the config, creates it if not found
+    # Поменял файл загрузки конфига на RAILS_ROOT/config/piwik.yml
       def self.load_config_from_file
         config = {}
-        home = ENV['HOME'] || ENV['USERPROFILE'] || ENV['HOMEPATH']
-        if File.exists?(home + "/.piwik")
-          temp_config = YAML::load open(home + "/.piwik")
+        if  File.exist?(File.join(RAILS_ROOT, 'config', 'piwik.yml'))
+          temp_config = YAML.load(File.read(File.join(RAILS_ROOT, 'config', 'piwik.yml')))[Rails.env]
         else
-          open(home + '/.piwik','w') { |f| f.puts @@template }
+          open(File.join(RAILS_ROOT, 'config', 'piwik.yml'),'w') { |f| f.puts @@template }
           temp_config = YAML::load(@@template)
         end
         temp_config.each { |k,v| config[k.to_sym] = v } if temp_config
         if config[:piwik_url] == nil || config[:auth_token] == nil
-          raise MissingConfiguration, "Please edit ~/.piwik to include your piwik url and auth_key"
+          raise MissingConfiguration, "Please edit config/piwik.yml to include your piwik url and auth_key"
         end
         config
       end
--- lib/piwik/site.rb
+++ lib/piwik/site.rb
@@ -2,14 +2,14 @@ module Piwik
   class Site < Piwik::Base
     attr_accessor :name, :main_url
     attr_reader :id, :created_at, :config
-    
-    # Initializes a new <tt>Piwik::Site</tt> object, with the supplied attributes. 
-    # 
-    # You can pass the URL for your Piwik install and an authorization token as 
-    # the second and third parameters. If you don't, than it will try to find 
-    # them in a <tt>'~/.piwik'</tt> (and create the file with an empty template if it 
+
+    # Initializes a new <tt>Piwik::Site</tt> object, with the supplied attributes.
+    #
+    # You can pass the URL for your Piwik install and an authorization token as
+    # the second and third parameters. If you don't, than it will try to find
+    # them in a <tt>'~/.piwik'</tt> (and create the file with an empty template if it
     # doesn't exists).
-    # 
+    #
     # Valid (and required) attributes are:
     # * <tt>:name</tt> - the site's name
     # * <tt>:main_url</tt> - the site's url
@@ -22,15 +22,15 @@ module Piwik
       end
       load_attributes(attributes)
     end
-  
-    # Returns an instance of <tt>Piwik::Site</tt> representing the site identified by 
-    # the supplied <tt>site_id</tt>. Raises a <tt>Piwik::ApiError</tt> if the site doesn't 
-    # exists or if the user associated with the supplied auth_token does not 
+
+    # Returns an instance of <tt>Piwik::Site</tt> representing the site identified by
+    # the supplied <tt>site_id</tt>. Raises a <tt>Piwik::ApiError</tt> if the site doesn't
+    # exists or if the user associated with the supplied auth_token does not
     # have at least 'view' access to the site.
-    # 
-    # You can pass the URL for your Piwik install and an authorization token as 
-    # the second and third parameters. If you don't, than it will try to find 
-    # them in a <tt>'~/.piwik'</tt> (and create the file with an empty template if it 
+    #
+    # You can pass the URL for your Piwik install and an authorization token as
+    # the second and third parameters. If you don't, than it will try to find
+    # them in a <tt>'~/.piwik'</tt> (and create the file with an empty template if it
     # doesn't exists).
     def self.load(site_id, piwik_url=nil, auth_token=nil)
       raise ArgumentError, "expected a site Id" if site_id.nil?
@@ -42,21 +42,21 @@ module Piwik
       attributes = get_site_attributes_by_id(site_id, @config[:piwik_url], @config[:auth_token])
       new(attributes, @config[:piwik_url], @config[:auth_token])
     end
-    
+
     # Returns <tt>true</tt> if the current site does not exists in the Piwik yet.
     def new?
       id.nil? && created_at.nil?
     end
-    
+
     # Saves the current site in Piwik.
-    # 
+    #
     # Calls <tt>create</tt> it it's a new site, <tt>update</tt> otherwise.
     def save
       new? ? create : update
     end
-    
+
     # Saves the current new site in Piwik.
-    # 
+    #
     # Equivalent Piwik API call: SitesManager.addSite (siteName, urls)
     def create
       raise ArgumentError, "Site already exists in Piwik, call 'update' instead" unless new?
@@ -68,9 +68,9 @@ module Piwik
       @created_at = Time.current
       id && id > 0 ? true : false
     end
-    
+
     # Saves the current site in Piwik, updating it's data.
-    # 
+    #
     # Equivalent Piwik API call: SitesManager.updateSite (idSite, siteName, urls)
     def update
       raise UnknownSite, "Site not existent in Piwik yet, call 'save' first" if new?
@@ -80,13 +80,13 @@ module Piwik
       result = XmlSimple.xml_in(xml, {'ForceArray' => false})
       result['success'] ? true : false
     end
-    
+
     def reload
       #TODO
     end
-    
+
     # Deletes the current site from Piwik.
-    # 
+    #
     # Equivalent Piwik API call: SitesManager.deleteSite (idSite)
     def destroy
       raise UnknownSite, "Site not existent in Piwik yet, call 'save' first" if new?
@@ -95,34 +95,41 @@ module Piwik
       freeze
       result['success'] ? true : false
     end
-    
+
     # Gives read access (<tt>'view'</tt>) to the supplied user login for the current
     # site.
     def give_view_access_to(login)
       give_access_to(:view, login)
     end
-    
-    # Gives read and write access (<tt>'admin'</tt>) for the supplied user login for the 
+
+    # Gives read and write access (<tt>'admin'</tt>) for the supplied user login for the
     # current site.
     def give_admin_access_to(login)
       give_access_to(:admin, login)
     end
-    
-    # Removes all access (gives an <tt>'noaccess'</tt>) for the supplied user login for 
+
+    # Removes all access (gives an <tt>'noaccess'</tt>) for the supplied user login for
     # the current site.
     def give_no_access_to(login)
       give_access_to(:noaccess, login)
     end
     alias_method :remove_access_from, :give_no_access_to
-    
-    # Returns a hash with a summary of access information for the current site 
-    # (visits, unique visitors, actions / pageviews, maximum actions per visit, 
-    # bounces and total time spent in all visits in seconds), filtered by the 
+
+
+    def get_page_urls(period=:day, date=Date.today)
+      raise UnknownSite, "Site not existent in Piwik yet, call 'save' first" if new?
+      xml = call('Actions.getPageUrls', :idSite => id, :period => period, :date => date, :idSubtable => '')
+      result = XmlSimple.xml_in(xml, {'ForceArray' => false}) 
+    end
+
+    # Returns a hash with a summary of access information for the current site
+    # (visits, unique visitors, actions / pageviews, maximum actions per visit,
+    # bounces and total time spent in all visits in seconds), filtered by the
     # supplied period and date.
-    # 
+    #
     # * <tt>period</tt> should be one of <tt>:day</tt>, <tt>:week</tt>, <tt>:month</tt> or <tt>:year</tt> (default: <tt>:day</tt>)
     # * <tt>date</tt> should be a <tt>Date</tt> object (default: <tt>Date.today</tt>)
-    # 
+    #
     # Equivalent Piwik API call: VisitsSummary.get (idSite, period, date)
     def summary(period=:day, date=Date.today)
       raise UnknownSite, "Site not existent in Piwik yet, call 'save' first" if new?
@@ -137,13 +144,13 @@ module Piwik
         :total_time_spent => result['sum_visit_length'].to_i, # in seconds
       }
     end
-    
-    # Returns the amount of visits for the current site, filtered by the 
+
+    # Returns the amount of visits for the current site, filtered by the
     # supplied period and date.
-    # 
+    #
     # * <tt>period</tt> should be one of <tt>:day</tt>, <tt>:week</tt>, <tt>:month</tt> or <tt>:year</tt> (default: <tt>:day</tt>)
     # * <tt>date</tt> should be a <tt>Date</tt> object (default: <tt>Date.today</tt>)
-    # 
+    #
     # Equivalent Piwik API call: VisitsSummary.getVisits (idSite, period, date)
     def visits(period=:day, date=Date.today)
       raise UnknownSite, "Site not existent in Piwik yet, call 'save' first" if new?
@@ -151,13 +158,13 @@ module Piwik
       result = XmlSimple.xml_in(xml, {'ForceArray' => false})
       result.to_i
     end
-    
-    # Returns the amount of unique visitors for the current site, filtered by 
+
+    # Returns the amount of unique visitors for the current site, filtered by
     # the supplied period and date.
-    # 
+    #
     # * <tt>period</tt> should be one of <tt>:day</tt>, <tt>:week</tt>, <tt>:month</tt> or <tt>:year</tt> (default: <tt>:day</tt>)
     # * <tt>date</tt> should be a <tt>Date</tt> object (default: <tt>Date.today</tt>)
-    # 
+    #
     # Equivalent Piwik API call: VisitsSummary.getUniqueVisitors (idSite, period, date)
     def unique_visitors(period=:day, date=Date.today)
       raise UnknownSite, "Site not existent in Piwik yet, call 'save' first" if new?
@@ -165,13 +172,13 @@ module Piwik
       result = XmlSimple.xml_in(xml, {'ForceArray' => false})
       result.to_i
     end
-    
-    # Returns the amount of actions (pageviews) for the current site, filtered 
+
+    # Returns the amount of actions (pageviews) for the current site, filtered
     # by the supplied period and date.
-    # 
+    #
     # * <tt>period</tt> should be one of <tt>:day</tt>, <tt>:week</tt>, <tt>:month</tt> or <tt>:year</tt> (default: <tt>:day</tt>)
     # * <tt>date</tt> should be a <tt>Date</tt> object (default: <tt>Date.today</tt>)
-    # 
+    #
     # Equivalent Piwik API call: VisitsSummary.getActions (idSite, period, date)
     def actions(period=:day, date=Date.today)
       raise UnknownSite, "Site not existent in Piwik yet, call 'save' first" if new?
@@ -180,17 +187,17 @@ module Piwik
       result.to_i
     end
     alias_method :pageviews, :actions
-    
+
     private
       # Calls the supplied Piwik API method, with the supplied parameters.
-      # 
-      # Returns a string containing the XML reply from Piwik, or raises a 
-      # <tt>Piwik::ApiError</tt> exception with the error message returned by Piwik 
+      #
+      # Returns a string containing the XML reply from Piwik, or raises a
+      # <tt>Piwik::ApiError</tt> exception with the error message returned by Piwik
       # in case it receives an error.
       def call(method, params={})
         self.class.call(method, params, config[:piwik_url], config[:auth_token])
       end
-    
+
       # Loads the attributes in the instance variables.
       def load_attributes(attributes)
         @id = attributes[:id]
@@ -200,10 +207,10 @@ module Piwik
       end
 
       # Gives the supplied access for the supplied user, for the current site.
-      # 
+      #
       # * <tt>access</tt> can be one of <tt>:view</tt>, <tt>:admin</tt> or <tt>:noaccess</tt>
       # * <tt>login</tt> is the user login on Piwik
-      # 
+      #
       # Equivalent Piwik API call: UsersManager.setUserAccess (userLogin, access, idSites)
       def give_access_to(access, login)
         raise UnknownSite, "Site not existent in Piwik yet, call 'save' first" if new?
@@ -213,9 +220,9 @@ module Piwik
       end
 
       # Calls the supplied Piwik API method, with the supplied parameters.
-      # 
-      # Returns a string containing the XML reply from Piwik, or raises a 
-      # <tt>Piwik::ApiError</tt> exception with the error message returned by Piwik 
+      #
+      # Returns a string containing the XML reply from Piwik, or raises a
+      # <tt>Piwik::ApiError</tt> exception with the error message returned by Piwik
       # in case it receives an error.
       def self.call(method, params={}, piwik_url=nil, auth_token=nil)
         raise MissingConfiguration, "Please edit ~/.piwik to include your piwik url and auth_key" if piwik_url.nil? || auth_token.nil?
@@ -233,9 +240,9 @@ module Piwik
         xml
       end
 
-      # Returns a hash with the attributes of the supplied site, identified 
+      # Returns a hash with the attributes of the supplied site, identified
       # by it's Id in <tt>site_id</tt>.
-      # 
+      #
       # Equivalent Piwik API call: SitesManager.getSiteFromId (idSite)
       def self.get_site_attributes_by_id(site_id, piwik_url, auth_token)
         xml = call('SitesManager.getSiteFromId', {:idSite => site_id}, piwik_url, auth_token)
